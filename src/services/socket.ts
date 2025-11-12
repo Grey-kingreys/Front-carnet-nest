e2d6c562c4e8d5a7dcced66ef1fd5c76ef8abc49
unknown <soulmamoudou0@gmail.com>
2025-11-12 09:47:44 +0000

    Apres recularisation des message

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -4,7 +4,7 @@ let socket: Socket | null = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io('http://localhost:8002', {
+    socket = io('http://localhost:9000', {
       path: '/socket.io',
       transports: ['websocket', 'polling'],
       autoConnect: true,
